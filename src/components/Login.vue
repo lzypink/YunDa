ad2368d1a0024ec90df84f6d1fb00cf0b96fda64
liuziyuan <1204386020@qq.com>
2019-09-21 15:42:42 +0800

    ttt

---
+++ src/components/Login.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="login_container">
+    <div class="login_boX">
+      <!-- 头像区域 -->
+      <div class="avater_box">
+        <img :src='ssr' alt="">
+
+      </div>
+      <!-- 表单区域 -->
+      <el-form ref='loginFormRef' :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" type="password" prefix-icon="iconfont icon-lock_fill"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" >登录</el-button>
+          <el-button type="info" >重置</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loginForm: {
+          username: '',
+          password:'',
+        },
+        loginFormRules: {
+          username: [
+            { required: true, message: '请输入登录名称', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入登录密码', trigger: 'blur' },
+            { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          ]
+        },
+        ssr: require('../assets/logo.png')
+      }
+    },
+    methods: {
+      restLoginForm() {
+        // console.log(this);
+        this.$refs.loginFormRef.resetFields()
+      },
+      login() {
+        this.$refs.loginFormRef.validate( async valid => {
+          if(!valid) return;
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if(res.meta.status != 200) return this.$message.error('登录失败')
+          this.$message.success('登录成功')
+          // 把登录成功之后服务器返回的token保存到客户端的sessionStorage中
+          window.sessionStorage.setItem('token', res.data.token)
+          // 通过编程式导航跳转到后台主页
+          this.$router.push('/home')
+        })
+      }
+    }
+  }
+</script>
+
+<style lang='less' scoped>
+  .login_container{
+    height: 100%;
+    background-color: #2b4b6d;
+  }
+
+  .login_boX {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 450px;
+    height: 300px;
+    background: #fff;
+    border-radius: 10px;
+    .avater_box {
+      height: 130px;
+      width: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 2px 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background: #eee;
+      }
+    }
+  }
+
+  .login_form{
+    position: absolute;
+    bottom: 10px;
+    width: 100%;
+    padding: 0 25px;
+    box-sizing: border-box;
+  }
+
+  .btns{
+    display: flex;
+    justify-content: flex-end;
+  }
+
+
+</style>

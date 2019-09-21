1059e58fc25dcc791b9d1f345980cfdcfaa9086d
liuziyuan <1204386020@qq.com>
2019-09-21 15:02:45 +0800

    15.02

--- src/router.js
+++ src/router.js
@@ -1,19 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+// import Home from './views/Home.vue'
+import Login from './components/Login.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home,
+    //   redirect: '/welcome',
+    // },
+    { path: '/', redirect: '/login'},
+    { path: '/login', component: Login},
     {
       path: '/about',
       name: 'about',
+
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
